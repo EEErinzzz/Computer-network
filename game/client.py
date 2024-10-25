dbc22b790f649817bc2c77402aeea90170554963
EEErinzzz <116625261+EEErinzzz@users.noreply.github.com>
2024-10-25 17:32:40 +0800

    Update client.py
    Authentication is done

--- game/client.py
+++ game/client.py
@@ -1,59 +1,52 @@
-from socket import *
+import socket
 import sys
 
-setdefaulttimeout(15)
+AUTHENTICATING = 0
+IN_GAME_HALL = 1
 
-def main(args):
-    server_address = args[1]
-    listen_port = args[2]
-    
+def main(server_ip, server_port):
+    state = AUTHENTICATING
+    """Main function to connect to the server and interact with it."""
     try:
-        client_socket = socket(AF_INET, SOCK_STREAM)
-        client_socket.connect((server_address, int(listen_port)))
-    except timeout as emsg:
-        print("Connection timeout! Please enter a correct Hostname or IP address. Error message: ", emsg)
-        sys.exit(1)
-    except error as emsg:
-            print("Socket error: ", emsg)
-            sys.exit(1)
-    except KeyboardInterrupt:
-            print("You terminate this process")
-            sys.exit(1)
-    
-    while True:
-        username = input("Please input your user name: ")
-        pwd = input("Please input your password: ")
+        # Create a socket to connect to the server
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect((server_ip, server_port))
+        print("Connected to the server.")
 
-        auth_msg = f"/login {username} {pwd}"
-        client_socket.send(auth_msg.encode())
+        while True:
+            if state == AUTHENTICATING:
+            # Receive ask for name message
+                message = client_socket.recv(1024).decode()
+                print(message)
+                username = input()
+            # Get user name and send it to the server
+                client_socket.send(username.encode())
+            #Receive ask for key message
+                message = client_socket.recv(1024).decode()
+                print(message)
+                userkey = input()
+                client_socket.send(userkey.encode())
+            #Receive authenticating result message
+                message = client_socket.recv(1024).decode()
+                if(message == "1001 Authentication successful\n"):
+                    state = IN_GAME_HALL
+                    print(message)
+                else:
+                    print(message)
+            
+            if state == IN_GAME_HALL:
+                pass
+            
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        client_socket.close()
 
-        #print("From server:", client_socket.recv(50)) # For checking
-        login_reply = client_socket.recv(50).decode() # Result of login         
-
-        if login_reply.split(" ")[0] == "1001":
-            print(login_reply)
-            break
-        else:
-             print(login_reply)
-    
-    ingame = False
-
-    while True:
-        msg_to_server = input()
-        client_socket.send(msg_to_server.encode())
-
-        server_msg = client_socket.recv(50).decode()
-        print(server_msg)
-        if server_msg.split(" ")[0] == "4001":
-            client_socket.close()
-            sys.exit(1)
-             
-             
-
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print("Usage: python3 client.py <Host_name> <Listen_port>")
+        print("Usage: python3 GameClient.py <Server_IP> <Server_Port>")
         sys.exit(1)
-    main(sys.argv)
+
+    server_ip = sys.argv[1]  # Server IP address
+    server_port = int(sys.argv[2])  # Server port number
+    main(server_ip, server_port)
